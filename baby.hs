adde3a9e980d1afbbcf6020dc23168fca9a81426
morimoto0606 <morimoto0606@yahoo.co.jp>
2017-06-25 09:38:01 +0900

    commit for second seminer

--- baby.hs
+++ baby.hs
@@ -4,3 +4,25 @@ doubleSmallNumber x = if x > 100
                   then x
                   else x*2
 doubleSmallNumber' x =  (doubleSmallNumber x) + 1
+
+removeNoUppercase :: [Char] -> [Char]
+removeNoUppercase st = [c |c<-st, c `elem` ['A'..'Z']]
+
+addThree x y z = x + y + z
+
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+lucky :: Int -> String
+lucky 7 = "Lucky Number Seven!"
+lucky x = "Sorry youre out of luck!"
+
+sayMe :: Int -> String
+SayMe 1 = "One"
+SayMe 2 = "Two"
+SayMe x = "not one nor two"
